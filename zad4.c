d9b08ea63cf2116953c64312f9c700d5949ec1dc
Lukasz Hoffman <highkillyou@gmail.com>
2016-11-05 11:32:09 +0100

    poprawione zadania

--- zad4.c
+++ zad4.c
@@ -4,11 +4,22 @@
 
 */
 
-int silnia(int k) {
-    return 0;
-}
-#include <stdio.h>
-int main() {
 
-printf("silnia(4) = %d\n", silnia(4));
+#include <stdio.h>
+
+
+
+int silnia(int k) {
+  int s = 1;
+  for(int n = 1; n<= k; n++) {
+    s = s * n;
+  }
+  return s;
+  }
+  int main () {
+    printf("silnia(0) = %d\n", silnia(0));
+    printf("silnia(1) = %d\n", silnia(1));
+    printf("silnia(2) = %d\n", silnia(2));
+    printf("silnia(3) = %d\n", silnia(3));
+    printf("silnia(4) = %d\n", silnia(4));
 }
